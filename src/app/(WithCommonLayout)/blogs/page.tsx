3e5041eed1228ec62bcc6d9739787ff89084723d
programarreza <programarreza@gmail.com>
2024-12-27 18:03:10 +0600

    blog page done

--- src/app/(WithCommonLayout)/blogs/page.tsx
+++ src/app/(WithCommonLayout)/blogs/page.tsx
@@ -1,9 +1,43 @@
+"use client";
+
+import { ImSpinner6 } from "react-icons/im";
+
 import Container from "@/src/components/Container";
+import BlogCard from "@/src/components/dashboard/blogs/BlogCard";
+import { useGetBlogs } from "@/src/hooks/blog.hook";
 
 const BlogsPage = () => {
+  const { data, isLoading } = useGetBlogs();
+  const blogs = data?.data?.result || [];
+
   return (
     <Container>
-      <h2 className="pt-24">Blogs page from home </h2>
+      {/* <div className="min-h-screen pt-24">
+        <BlogCard blogs={blogs} />
+      </div> */}
+
+      <div>
+        {isLoading ? (
+          <div className="flex justify-center items-center min-h-screen">
+            <div className="flex w-fit mx-auto">
+              <ImSpinner6 className="animate-spin m-auto" size={28} />
+              <span>Loading...</span>
+            </div>
+          </div>
+        ) : (
+          <>
+            {data?.data?.result?.length === 0 ? (
+              <p className="flex justify-center items-center min-h-screen my-auto text-xl font-medium">
+                blog not aboailvale
+              </p>
+            ) : (
+              <div className="min-h-screen pt-24">
+                <BlogCard blogs={blogs} />
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </Container>
   );
 };
---
+++ src/components/dashboard/blogs/BlogCard.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const BlogCard = ({ blogs }: { blogs: any }) => {
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-3">
+        {blogs.map((blog: any) => (
+          <Link key={blog?._id} passHref href={`/blogs/${blog?._id}`}>
+            <div
+              key={blog?._id}
+              className="shadow-large border border-gray-500 flex flex-col justify-between h-full p-4 rounded-lg"
+            >
+              <div
+                dangerouslySetInnerHTML={{
+                  __html:
+                    blog?.content?.length > 70
+                      ? blog?.content?.slice(0, 70) + "."
+                      : blog?.content,
+                }}
+                className="prose prose-invert max-w-none text-white text-xl overflow-hidden text-ellipsis line-clamp-3 mb-4"
+              />
+              <div className="mt-auto">
+                <Image
+                  alt={blog?._id}
+                  className=" w-72 h-52 object-cover"
+                  height={200}
+                  src={blog?.image}
+                  width={200}
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
